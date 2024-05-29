3c1a571baad3437784ca046899efcc102a615ceb
Yashswijain85 <137017545+Yashswijain85@users.noreply.github.com>
2024-05-29 14:46:44 +0530

    Update Students_and_Examinations.sql

--- Students_and_Examinations.sql
+++ Students_and_Examinations.sql
@@ -1,3 +1,5 @@
+-- A cross join is a type of join that returns the Cartesian product of rows from the tables in the join. In other words, it combines each row from the first table with each row from the second table.
+
 SELECT Students.student_id, Students.student_name, Subjects.subject_name, COUNT(Examinations.subject_name) as attended_exams
 FROM Students 
 CROSS JOIN Subjects 
@@ -5,3 +7,4 @@ LEFT JOIN Examinations
 ON Students.student_id = Examinations.student_id AND Subjects.subject_name = Examinations.subject_name
 GROUP BY Students.student_id, Students.student_name, Subjects.subject_name
 ORDER BY Students.student_id, Subjects.subject_name
+
